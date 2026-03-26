5e8bc02c089c41f40f55224a0a36d0b0e21681d2
aqibali541k <aqibali541k@gmail.com>
2026-03-26 08:12:24 -0700

    fix url

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://bmigo.vercel.app"], // Removed /
+    origin: ["https://bmigo.vercel.app"], // Removed /
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
